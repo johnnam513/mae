2637564e59ddbba131e6f0cd694baee8202a27fe
Jaehyun Nam <60807232+johnnam513@users.noreply.github.com>
2022-04-17 22:49:47 +0900

    Update main_pretrain.py

--- main_pretrain.py
+++ main_pretrain.py
@@ -75,6 +75,7 @@ def get_args_parser():
     # Dataset parameters
     parser.add_argument('--data_path', default='/datasets01/imagenet_full_size/061417/', type=str,
                         help='dataset path')
+    parser.add_argument('--data_root', type=str, help='path to the training images')
 
     parser.add_argument('--output_dir', default='./output_dir',
                         help='path where to save, empty for no saving')
